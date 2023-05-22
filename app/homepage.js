11ed04a935f0ccad8a696b7a7b9d9ba27127ade5
datmaiz <ngodat02012004@gmail.com>
2023-05-22 10:56:14 +0700

    responsive

--- app/homepage.js
+++ app/homepage.js
@@ -1,1 +1,15 @@
+const leftMenuButton = document.querySelector('.header-nav-left-menus');
+const rightMenuButton = document.querySelector('.header-nav-right-menus');
+const leftMenu = document.querySelector('.nav.header-nav-left');
+const rightMenu = document.querySelector('.nav.header-nav-right');
+
+leftMenuButton.addEventListener('click', () => {
+    leftMenuButton.classList.toggle('open');
+    leftMenu.classList.toggle('open');
+})
+
+rightMenuButton.addEventListener('click', () => {
+    rightMenuButton.classList.toggle('open');
+    rightMenu.classList.toggle('open');
+})
 
